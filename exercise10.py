5d7baebc012cee9ca683bc9ae41eeb37d74b81f4
SAICHARAN <32188282+SAICHARAN007@users.noreply.github.com>
2020-01-09 13:53:31 +0530

    basics

---
+++ exercise10.py
@@ -0,0 +1,16 @@
+college="\t CBIT"
+coureses="Btech \n mca \n mtech"
+dept="""
+\t\t\t Departments\n
+\t Btech
+\t\t *cse
+\t\t *IT
+\t\t *mech
+\t\t *civil
+\t\t *production
+\t mca
+\t mtech
+""""
+print("college")
+print("courses")
+print("dept")
