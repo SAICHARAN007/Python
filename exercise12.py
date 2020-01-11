0b16858ddc51ad0ddf1a01c8cd3f83f0cf3aa775
SAICHARAN <32188282+SAICHARAN007@users.noreply.github.com>
2020-01-11 22:31:15 +0530

    basics

---
+++ exercise12.py
@@ -0,0 +1,4 @@
+user_name=input('enter the name')
+mail_id=input('enter mail id')
+password=input('enter password')
+print('the given credintials',user_name,mail_id,password,'all are mateched, so provide the access.')
---
+++ exercise13.py
@@ -0,0 +1,10 @@
+#parameters/unpacking/variables
+from sys import argv
+starters,main_course,kababs,deserts=argv
+print('the first we have to start with',script)
+print('then next order the',main_course)
+print('to feel get feel of taste',kababs)
+print('but finally we have to end with',deserts)
+#the above code save as exercise13.py
+#write the code as per mentioned below
+$ python3.6 ex13.py starters,main_course,kababs,deserts
