bca7393eaa70b299460caaa3c12ba8de3a0e7d22
SAICHARAN <32188282+SAICHARAN007@users.noreply.github.com>
2020-01-24 19:38:04 +0530

    fun basics

---
+++ exercise18.py
@@ -0,0 +1,10 @@
+#functions 
+def print(*info):
+    name, contact=info
+    print(f"the information details are name:{name} and contact:{contact}")
+def print(name, contact):
+    name,contact=info
+    print(f"the information details are name:{name} and contact:{contact}")
+#this one takes no arguments
+def print_none():
+    print("I got nothing'.")
