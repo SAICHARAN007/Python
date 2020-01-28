c2d07b87fc263240a0a01de9b7d24bc813e1b6da
SAICHARAN <32188282+SAICHARAN007@users.noreply.github.com>
2020-01-28 13:13:08 +0530

    fun basics

---
+++ exercise19.py
@@ -0,0 +1,18 @@
+def NYC(no_of_stags, no_of_couples):
+    print(f"there is {no_of_stags} no.of stags and {no_of_couples} no.of couples are coming")
+    print(f"the total count of {no_of_stags+no_of_couples} are enough to success the party")
+    print("""
+          let's say HAPPY NEW YEAR
+          Hurry
+          hurry
+          """)
+print("now give the exact count of participants")
+NYC(95,100)
+
+print("now give approx count along spot registertions")
+no_of_stags=125
+no_of_couples=150
+NYC(no_of_stags, no_of_couples)
+
+print("the count after giving some free passes")
+NYC(125+15, 150+10)
